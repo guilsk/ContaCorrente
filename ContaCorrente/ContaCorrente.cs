219d781180deea01ed4ebded8f8a36ef907e2b40
guilsk <guilherme.gmdp@gmail.com>
2023-04-10 13:59:21 -0300

    Add project files.

---
+++ ContaCorrente/ContaCorrente.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaCorrente
+{
+    internal class ContaCorrente
+    {
+        public int numero;
+        public double saldo;
+        public bool isSpecial;
+        public double limite;
+        public string[] movimentacao;
+
+
+
+        public ContaCorrente(int numero, double saldo, bool isSpecial, double limite, string[] movimentacao)
+        {
+            this.numero = numero;
+            this.saldo = saldo;
+            this.isSpecial = isSpecial;
+            this.limite = limite;
+            this.movimentacao = movimentacao;
+        }
+
+        public ContaCorrente()
+        {
+        }
+
+        public void Sacar(double valor)
+        {
+            if(valor <= this.limite + this.saldo) this.saldo -= valor;
+        }
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public void ExibirSaldo()
+        {
+            Console.WriteLine("Saldo: " + this.saldo);
+        }
+
+        public void ExibirExtrato()
+        {
+            Console.WriteLine("Número: " + this.numero);
+            Console.WriteLine("Saldo: " + this.saldo);
+            Console.WriteLine("Conta Especial: " + this.isSpecial);
+            Console.WriteLine("Limite: " + this.limite);
+            Console.WriteLine("Movimentação:");
+            for (int i = 0; i < this.movimentacao.Length; i++){
+                if(i % 2 == 0)
+                    movimentacao[i] = "credito";
+                else
+                    movimentacao[i] = "débito";
+                Console.WriteLine("\t" + this.movimentacao[i]);
+            }
+        }
+
+        public void TransferirPara(ContaCorrente c, double valor)
+        {
+            c.saldo += valor;
+        }
+    }
+}
---
+++ ContaCorrente/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ContaCorrente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente c1 = new ContaCorrente();
+            c1.saldo = 1000;
+            c1.numero = 12;
+            c1.limite = 0;
+            c1.isSpecial = true;
+            c1.movimentacao = new string[10];
+
+            c1.Sacar(200);
+            c1.Depositar(300);
+            c1.Depositar(500);
+            c1.Sacar(200);
+
+            ContaCorrente c2 = new ContaCorrente();
+            c2.saldo = 300;
+            c2.numero = 13;
+            c2.limite = 0;
+            c2.isSpecial = true;
+            c2.movimentacao = new string[10];
+
+            c1.TransferirPara(c2, 400);
+            c1.ExibirExtrato();
+        }
+    }
+}
